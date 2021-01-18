e9e30b326917a1adddb1c42427ba919edd65178a
Adebayo <adebayotobiloba9@gmail.com>
2021-01-18 08:51:14 +0100

    updated component

--- src/components/loading/loading.component.jsx
+++ src/components/loading/loading.component.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import './loading.styles.css'
 
 const Loading = () => {
-  return <div>LOADING</div>;
+  return (
+    <div className='loader'>
+    <div className='loading'></div></div>
+  );
 };
 
 export default Loading;
