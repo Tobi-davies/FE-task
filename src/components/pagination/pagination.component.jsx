64495049a46c0d9f43fb9250012a585da5fdcbaf
Adebayo <adebayotobiloba9@gmail.com>
2021-01-18 08:27:22 +0100

    changed styling

--- src/components/pagination/pagination.component.jsx
+++ src/components/pagination/pagination.component.jsx
@@ -1,48 +1,3 @@
-// import React from "react";
-
-// const Pagination = () => {
-//   const pageLinks = [];
-
-//   for (let i = 1; i < props.pages + 1; i++) {
-//     let active = props.currentPage == i ? "active" : "";
-
-//     pageLinks.push(
-//       <li
-//         className={`waves-effect ${active}`}
-//         key={i}
-//         onClick={() => props.nextPage(i)}
-//       >
-//         [i]
-//       </li>
-//     );
-//   }
-
-//   return (
-//     <div className="container">
-//       <div className="container-row">
-//         <ul className="pagination">
-//           {props.currentPage > 1 ? (
-//             <li className={`waves-effect`} onClick={() => props.nextPage(i)}>
-//               PREV
-//             </li>
-//           ) : (
-//             ""
-//           )}
-//           {pageLinks}
-//           {props.currentPage < props.pages + 1 ? (
-//             <li className={`waves-effect`} onClick={() => props.nextPage(i)}>
-//               NEXT
-//             </li>
-//           ) : (
-//             ""
-//           )}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Pagination;
 
 import React from "react";
 import "./pagination.styles.css";
@@ -57,7 +12,6 @@ const Pagination = ({ pageLimit, newLength, paginate, currentPage }) => {
   console.log(averagePost);
 
   return (
-    // <div className="pagination-container">
     <nav className="paginated">
       {currentPage > 1 ? (
         <li className="prev" onClick={() => paginate(currentPage - 1)}>
@@ -80,7 +34,6 @@ const Pagination = ({ pageLimit, newLength, paginate, currentPage }) => {
         ""
       )}
     </nav>
-    // </div>
   );
 };
 
